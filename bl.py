01e00a3cf88a369e2824a19f248b444bda8dd37d
Mr.Magic <64154241+Retr09596@users.noreply.github.com>
2022-06-01 15:46:48 +0300

    Delete bl.py

--- bl.py
+++
@@ -1,8 +0,0 @@
-import bpy
-
-for scene in bpy.data.scenes:
-    scene.render.engine = 'BLENDER_EEVEE'
-
-bpy.context.scene.eevee.taa_render_samples = 2048
-
-bpy.ops.render.render(animation=False, write_still=True)
